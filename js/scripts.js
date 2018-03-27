2058795f5f0ded42d51a92ee6fcc6b85cef9601e
Brea Borlas and Dawn Mott <brea@>
2018-03-27 10:32:15 -0700

    add js for fortune quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,48 @@
 $(document).ready(function() {
-  $("form#transportation-survey").submit(function(event){
+  $("form#events").submit(function(event) {
     event.preventDefault();
-    $("#work-responses").show();
-    $("input:checkbox[name=work-transportation]:checked").each(function(){
-      var workTransportationMode = $(this).val();
-      $("#work-responses").append(workTransportationMode + "<br>");
+    $("#response").show();
+    var luckScore = 0;
+    $("input:checkbox[name=luckyEvents]:checked").each(function() {
+      var luckyEvent = parseInt($(this).val());
+      luckScore += luckyEvent;
     });
-    $("#fun-responses").show();
-    $("input:checkbox[name=fun-transportation]:checked").each(function(){
-      var funTransportationMode = $(this).val();
-      $('#fun-responses').append(funTransportationMode + "<br>");
+    $("input:checkbox[name=unluckyEvents]:checked").each(function() {
+      var unluckyEvent = parseInt($(this).val());
+      luckScore += unluckyEvent;
     });
-    $("#transportation-survey").hide();
+    var fortune;
+    if(luckScore >= -10 && luckScore < -3){
+      fortune = "You are very unlucky! Don't play the lottery today.";
+    }
+    else if (luckScore >= -3 && luckScore < 3){
+      fortune = "You make your own luck!";
+    }
+    else {
+      fortune = "Hooray you're so lucky! You'll have good luck with all your endeavors.";
+    }
+    $("#response").append(fortune);
+    $("#events").hide();
   });
 });
+
+
+// $(document).ready(function() {
+//   $("form#transportation-survey").submit(function(event){
+//     event.preventDefault();
+//     $("#work-responses").show();
+//     $("input:checkbox[name=work-transportation]:checked").each(function(){
+//       var workTransportationMode = $(this).val();
+//       $("#work-responses").append(workTransportationMode + "<br>");
+//     });
+//     $("#fun-responses").show();
+//     $("input:checkbox[name=fun-transportation]:checked").each(function(){
+//       var funTransportationMode = $(this).val();
+//       $('#fun-responses').append(funTransportationMode + "<br>");
+//     });
+//     $("#transportation-survey").hide();
+//   });
+// });
 
 
 
