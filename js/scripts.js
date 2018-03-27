9129d523eb97c051ebbf74dee9a9a7e83344adbf
Brea Borlas and Dawn Mott <dawn@>
2018-03-27 09:51:05 -0700

    Add transportation quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
+$(document).ready(function() {
+  $("form#transportation-survey").submit(function(event){
+    event.preventDefault();
+    $("#work-responses").show();
+    $("input:checkbox[name=work-transportation]:checked").each(function(){
+      var workTransportationMode = $(this).val();
+      $("#work-responses").append(workTransportationMode + "<br>");
+    });
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funTransportationMode = $(this).val();
+      $('#fun-responses').append(funTransportationMode + "<br>");
+    });
+    $("#transportation-survey").hide();
+  });
+});
+
+
+
+
+
+
+
 // $(document).ready(function() {
   // $("#countUp").submit(function(event){
   //
@@ -11,19 +34,19 @@
 //   });
 // });
 
-$(document).ready(function() {
-  $("#formOne").submit(function () {
-    var wordPuzzle = $("#puzzleSentence").val();
-    var vowels = "aeiou";
-    for(var index = 0; index <= wordPuzzle.length; index ++){
-      for(var vowelIndex = 0; vowelIndex <= vowels.length; vowelIndex ++) {
-        if (wordPuzzle[index] === vowels[vowelIndex]) {
-          wordPuzzle = wordPuzzle.replace(wordPuzzle[index], "-");
-        }
-      }
-    }
-    alert(wordPuzzle);
-  })
-
-
-});
+// $(document).ready(function() {
+//   $("#formOne").submit(function () {
+//     var wordPuzzle = $("#puzzleSentence").val();
+//     var vowels = "aeiou";
+//     for(var index = 0; index <= wordPuzzle.length; index ++){
+//       for(var vowelIndex = 0; vowelIndex <= vowels.length; vowelIndex ++) {
+//         if (wordPuzzle[index] === vowels[vowelIndex]) {
+//           wordPuzzle = wordPuzzle.replace(wordPuzzle[index], "-");
+//         }
+//       }
+//     }
+//     alert(wordPuzzle);
+//   })
+//
+//
+// });
